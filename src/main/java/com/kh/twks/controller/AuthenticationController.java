feba3e423343d7540a2f5099e4da3fd683d627ac
BaoXueyao <21jy0133@jec.ac.jp>
2023-02-26 22:47:44 +0900

    fix

--- src/main/java/com/kh/twks/controller/AuthenticationController.java
+++ src/main/java/com/kh/twks/controller/AuthenticationController.java
@@ -34,11 +34,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import java.nio.charset.Charset;
+import java.security.SecureRandom;
 import java.sql.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("/auth")
@@ -150,9 +153,9 @@ public class AuthenticationController {
 
         byte[] array = new byte[12]; // length is bounded by 7
         new Random().nextBytes(array);
-        String generatedString = new String(array, Charset.forName("UTF-8"));
+        RandomString gen = new RandomString(12, ThreadLocalRandom.current());
 
-        user.setPassword(generatedString);
+        user.setPassword(gen.nextString());
 
 
         user.setRankCode("01");
@@ -196,6 +199,63 @@ public class AuthenticationController {
 
 
         return ResponseEntity.ok(responseMap);
+    }
+
+}
+
+
+class RandomString {
+
+    /**
+     * Generate a random string.
+     */
+    public String nextString() {
+        for (int idx = 0; idx < buf.length; ++idx)
+            buf[idx] = symbols[random.nextInt(symbols.length)];
+        return new String(buf);
+    }
+
+    public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static final String lower = upper.toLowerCase(Locale.ROOT);
+
+    public static final String digits = "0123456789";
+
+    public static final String alphanum = upper + lower + digits;
+
+    private final Random random;
+
+    private final char[] symbols;
+
+    private final char[] buf;
+
+    public RandomString(int length, Random random, String symbols) {
+        if (length < 1) throw new IllegalArgumentException();
+        if (symbols.length() < 2) throw new IllegalArgumentException();
+        this.random = Objects.requireNonNull(random);
+        this.symbols = symbols.toCharArray();
+        this.buf = new char[length];
+    }
+
+    /**
+     * Create an alphanumeric string generator.
+     */
+    public RandomString(int length, Random random) {
+        this(length, random, alphanum);
+    }
+
+    /**
+     * Create an alphanumeric strings from a secure generator.
+     */
+    public RandomString(int length) {
+        this(length, new SecureRandom());
+    }
+
+    /**
+     * Create session identifiers.
+     */
+    public RandomString() {
+        this(21);
     }
 
 }
